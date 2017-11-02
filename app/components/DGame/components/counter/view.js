05b458c91a224369d4ada6a8d5e5b427e848fddf
Matthew Elphick <matt.a.elphy@gmail.com>
2017-11-02 07:38:05 +0000

    :wrench: Work on component component FE view

--- app/components/DGame/components/counter/view.js
+++ app/components/DGame/components/counter/view.js
@@ -1,3 +1,31 @@
 import React from 'react'
+import Typography from 'material-ui/Typography'
+import IconButton from 'material-ui/IconButton'
+import AddIcon from 'material-ui-icons/Add'
 
-export default () => (<div>Counter</div>)
+class Counter extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { counter: 0 }
+  }
+
+  increment () {
+    let { counter } = this.state
+    counter += 1
+    this.setState({ counter })
+  }
+
+  render () {
+    const { counter } = this.state
+    return (
+      <Typography type='display2'>
+          Counter: {counter}
+          <IconButton onClick={this.increment.bind(this)}>
+              <AddIcon />
+          </IconButton>
+      </Typography>
+    )
+  }
+}
+
+export default Counter
